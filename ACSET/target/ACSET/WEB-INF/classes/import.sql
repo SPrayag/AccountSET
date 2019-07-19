01bb2a862b745b537ccb10104a46f373ec648f97
SPrayag <Shalini.Prayag@academytrainee.com>
2019-07-19 09:00:37 +0100

    update2

---
+++ ACSET/target/ACSET/WEB-INF/classes/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNT (accountNo, firstName, lastName) VALUES ('1234', 'John', 'Doe');
+INSERT INTO ACCOUNT (accountNo, firstName, lastName) VALUES ('1235', 'Jane', 'Doe');
+INSERT INTO ACCOUNT (accountNo, firstName, lastName) VALUES ('1236', 'Jim', 'Taylor');
